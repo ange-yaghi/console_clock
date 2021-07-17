8af4aed4fa2ed93a42fc771a9dca289bc3996fcb
Ange Yaghi <me@angeyaghi.com>
2021-07-17 13:47:39 -0400

    Added wall clock

--- src/main.cpp
+++ src/main.cpp
@@ -289,6 +289,37 @@ bool is_inactive() {
     return true;
 }
 
+const char *get_month(int m) {
+    switch (m) {
+        case 0: return "JAN";
+        case 1: return "FEB";
+        case 2: return "MAR";
+        case 3: return "APR";
+        case 4: return "MAY";
+        case 5: return "JUN";
+        case 6: return "JUL";
+        case 7: return "AUG";
+        case 8: return "SEP";
+        case 9: return "OCT";
+        case 10: return "NOV";
+        case 11: return "DEC";
+        default: return "XXX";
+    }
+}
+
+const char *get_weekday(int i) {
+    switch (i) {
+        case 0: return "SUN";
+        case 1: return "MON";
+        case 2: return "TUE";
+        case 3: return "WED";
+        case 4: return "THU";
+        case 5: return "FRI";
+        case 6: return "SAT";
+        default: return "XXX";
+    }
+}
+
 void draw_status_indicators(
         struct screen_buffer *target,
         bool paused,
@@ -303,27 +334,57 @@ void draw_status_indicators(
 
     const COORD pos_0_0 = pos;
     const COORD pos_0_1 = { pos.X, (SHORT)(pos.Y + 1) }; 
-    const COORD pos_1_0 = { (SHORT)(pos.X + 16), pos.Y };
+    const COORD pos_1_0 = { (SHORT)(pos.X + 32), pos.Y };
+    const COORD pos_2_0 = { (SHORT)(pos.X + 32 + 16), pos.Y };
+    const COORD pos_0_2 = { pos.X, (SHORT)(pos.Y + 1) };
 
     const COORD pos_0 = pos;
     const COORD pos_1 = single_line
         ? pos_1_0
         : pos_0_1;
+    const COORD pos_2 = single_line
+        ? pos_2_0
+        : pos_0_2;
 
     if (paused) {
-        ::draw_text(status_paused, target, pos_1, BACKGROUND_RED);
+        ::draw_text(status_paused, target, pos_2, BACKGROUND_RED);
     }
 
     if (active) {
-        ::draw_text(status_active, target, pos_0, BACKGROUND_BLUE);
+        ::draw_text(status_active, target, pos_1, BACKGROUND_BLUE);
     }
     else {
         ::draw_text(
                 status_idle,
                 target,
-                pos_0,
+                pos_1,
                 BACKGROUND_RED | BACKGROUND_GREEN);
     }
+
+    time_t t = time(NULL);
+    struct tm time_data = *localtime(&t);
+    char current_time[256];
+
+    const char *am_pm = (time_data.tm_hour >= 12)
+        ? "PM"
+        : "AM";
+    const int hour = (time_data.tm_hour == 12)
+        ? 12
+        : (time_data.tm_hour + 12) % 24;
+
+    sprintf(
+            current_time,
+            "%s %s %d, %d | %d:%d %s",
+            get_weekday(time_data.tm_wday),
+            get_month(time_data.tm_mon),
+            time_data.tm_mday,
+            time_data.tm_year + 1900,
+            hour,
+            time_data.tm_min,
+            am_pm);
+
+    ::draw_text(current_time, target, pos_0, default_color);
+
 }
 
 void on_exit(HANDLE console_handle, SHORT width, SHORT height) {

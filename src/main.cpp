7218aeae6011dec79d1a452fe8cfa5255783f648
Ange Yaghi <me@angeyaghi.com>
2021-07-16 23:16:58 -0400

    Added single line mode

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,12 @@
 
 #include <console_clock.h>
 
+static const DWORD default_color =
+    FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY;
+
 struct screen_buffer {
     char *c;
+    DWORD *attributes;
     COORD size;
 };
 
@@ -52,11 +56,13 @@ struct timer_context {
 
 void init_buffer(screen_buffer *buffer, SHORT width, SHORT height) {
     buffer->c = (char *)malloc(sizeof(char) * width * height);
+    buffer->attributes = (DWORD *)malloc(sizeof(DWORD) * width * height);
     buffer->size = { width, height };
 }
 
 void free_buffer(screen_buffer *buffer) {
     free(buffer->c);
+    free(buffer->attributes);
 }
 
 void clear_buffer(screen_buffer *buffer, char c) {
@@ -65,7 +71,8 @@ void clear_buffer(screen_buffer *buffer, char c) {
 
     for (SHORT y = 0; y < height; ++y) {
         for (SHORT x = 0; x < width; ++x) {
-            buffer->c[y * buffer->size.X + x] = c; 
+            buffer->c[y * buffer->size.X + x] = c;
+            buffer->attributes[y * buffer->size.X + x] = default_color;
         }
     }
 }
@@ -81,8 +88,11 @@ size_t stripped_len(const char *s) {
     return 0;
 }
 
-int load_character(const char *fname, screen_buffer *buffer) {
-    FILE *fp = fopen(fname, "r");
+int load_character(const char *root_path, const char *fname, screen_buffer *buffer) {
+    char full_path[1024];
+    sprintf(full_path, "%s/%s", root_path, fname);
+    
+    FILE *fp = fopen(full_path, "r");
     size_t read;
     size_t len = 0;
     char line[256];
@@ -115,7 +125,7 @@ int load_character(const char *fname, screen_buffer *buffer) {
     return 1;
 }
 
-void load_characters() {
+void load_characters(const char *root_path) {
     c_0 = (struct screen_buffer *)malloc(sizeof(struct screen_buffer));
     c_1 = (struct screen_buffer *)malloc(sizeof(struct screen_buffer));
     c_2 = (struct screen_buffer *)malloc(sizeof(struct screen_buffer));
@@ -128,40 +138,65 @@ void load_characters() {
     c_9 = (struct screen_buffer *)malloc(sizeof(struct screen_buffer));
     c_colon = (struct screen_buffer *)malloc(sizeof(struct screen_buffer));
     
-    load_character("resources/big_money_nw/0.txt", c_0);
-    load_character("resources/big_money_nw/1.txt", c_1);
-    load_character("resources/big_money_nw/2.txt", c_2);
-    load_character("resources/big_money_nw/3.txt", c_3);
-    load_character("resources/big_money_nw/4.txt", c_4);
-    load_character("resources/big_money_nw/5.txt", c_5);
-    load_character("resources/big_money_nw/6.txt", c_6);
-    load_character("resources/big_money_nw/7.txt", c_7);
-    load_character("resources/big_money_nw/8.txt", c_8);
-    load_character("resources/big_money_nw/9.txt", c_9);
-    load_character("resources/big_money_nw/colon.txt", c_colon);
+    load_character(root_path, "resources/big_money_nw/0.txt", c_0);
+    load_character(root_path, "resources/big_money_nw/1.txt", c_1);
+    load_character(root_path, "resources/big_money_nw/2.txt", c_2);
+    load_character(root_path, "resources/big_money_nw/3.txt", c_3);
+    load_character(root_path, "resources/big_money_nw/4.txt", c_4);
+    load_character(root_path, "resources/big_money_nw/5.txt", c_5);
+    load_character(root_path, "resources/big_money_nw/6.txt", c_6);
+    load_character(root_path, "resources/big_money_nw/7.txt", c_7);
+    load_character(root_path, "resources/big_money_nw/8.txt", c_8);
+    load_character(root_path, "resources/big_money_nw/9.txt", c_9);
+    load_character(root_path, "resources/big_money_nw/colon.txt", c_colon);
 }
 
-void update_screen(HANDLE console_handle, screen_buffer *back_buffer, screen_buffer *front_buffer, bool force) {
+void update_screen(
+        HANDLE console_handle,
+        screen_buffer *back_buffer,
+        screen_buffer *front_buffer, 
+        bool force)
+{
     SHORT width = min(back_buffer->size.X, front_buffer->size.X);
     SHORT height = min(back_buffer->size.Y, front_buffer->size.Y);
 
     for (SHORT y = 0; y < height; ++y) {
         for (SHORT x = 0; x < width; ++x) {
-            if (front_buffer->c[y * width + x] != back_buffer->c[y * width + x] || force) {
+            const char f_c = front_buffer->c[y * width + x];
+            const char b_c = back_buffer->c[y * width + x];
+
+            const char f_attributes = front_buffer->attributes[y * width + x];
+            const char b_attributes = back_buffer->attributes[y * width + x];
+
+            if (force || f_c != b_c || f_attributes != b_attributes) {
+                SetConsoleTextAttribute(console_handle, f_attributes);
                 SetConsoleCursorPosition(console_handle, { x, y });
                 WriteConsoleA(console_handle, &front_buffer->c[y * width + x], 1, NULL, NULL);
             }
         }
     }
+
+    SetConsoleTextAttribute(console_handle, default_color);
 }
 
-void draw_text(const char *text, screen_buffer *buffer, COORD pos) {
+void draw_text(
+        const char *text,
+        screen_buffer *buffer,
+        COORD pos,
+        DWORD attributes,
+        int *right_edge = NULL)
+{
     if (pos.Y < 0 || pos.Y >= buffer->size.Y) return;
 
     for (SHORT i = 0; text[i] != 0; ++pos.X, ++i) {
         if (pos.X < 0 || pos.X >= buffer->size.X) continue;
         buffer->c[pos.Y * buffer->size.X + pos.X] = text[i];
-    } 
+        buffer->attributes[pos.Y * buffer->size.X + pos.X] = attributes;
+    }
+
+    if (right_edge != NULL) {
+        *right_edge = pos.X;
+    }
 }
 
 void blit(screen_buffer *target, screen_buffer *buffer, COORD pos) {
@@ -180,11 +215,12 @@ void blit(screen_buffer *target, screen_buffer *buffer, COORD pos) {
             if (y_f < 0 || y_f >= t_height) continue;
 
             target->c[y_f * t_width + x_f] = buffer->c[y * width + x];
+            target->attributes[y_f * t_width + x_f] = buffer->attributes[y * width + x];
         }
     }
 }
 
-void draw_large_text(const char *text, screen_buffer *target, COORD pos) {
+void draw_large_text(const char *text, screen_buffer *target, COORD pos, int *right_edge) {
     for (int i = 0; text[i] != NULL; ++i) {
         const char c = text[i];
         struct screen_buffer *glyph = get_glyph(c);
@@ -196,6 +232,8 @@ void draw_large_text(const char *text, screen_buffer *target, COORD pos) {
 
         pos.X += width;
     }
+
+    *right_edge = pos.X;
 }
 
 void show_cursor(HANDLE console_handle, bool show = true) {
@@ -251,39 +289,41 @@ bool is_inactive() {
     return true;
 }
 
-void draw_status_indicators(HANDLE console_handle, bool paused, bool active, COORD pos) {
+void draw_status_indicators(
+        struct screen_buffer *target,
+        bool paused,
+        bool active,
+        bool single_line,
+        COORD pos)
+{
     static const char *status_paused    = " PAUSED         ";
     static const char *status_active    = " ACTIVE         ";
     static const char *status_idle      = " IDLE           ";
     static const char *status_empty     = "                ";
 
-    struct _CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
-    ::GetConsoleScreenBufferInfo(console_handle, &screen_buffer_info);
+    const COORD pos_0_0 = pos;
+    const COORD pos_0_1 = { pos.X, (SHORT)(pos.Y + 1) }; 
+    const COORD pos_1_0 = { (SHORT)(pos.X + 16), pos.Y };
+
+    const COORD pos_0 = pos;
+    const COORD pos_1 = single_line
+        ? pos_1_0
+        : pos_0_1;
 
     if (paused) {
-        SetConsoleTextAttribute(console_handle, BACKGROUND_RED);
-        SetConsoleCursorPosition(console_handle, { pos.X, (SHORT)(pos.Y + 1) });
-        ::WriteConsoleA(console_handle, status_paused, strlen(status_paused), NULL, NULL);
+        ::draw_text(status_paused, target, pos_1, BACKGROUND_RED);
     }
-    else {
-        SetConsoleTextAttribute(console_handle, screen_buffer_info.wAttributes);
-        SetConsoleCursorPosition(console_handle, { pos.X, (SHORT)(pos.Y + 1) });
-        ::WriteConsoleA(console_handle, status_empty, strlen(status_empty), NULL, NULL); 
-    }
-
 
     if (active) {
-        SetConsoleTextAttribute(console_handle, BACKGROUND_GREEN);
-        SetConsoleCursorPosition(console_handle, { pos.X, (SHORT)(pos.Y + 0) });
-        ::WriteConsoleA(console_handle, status_active, strlen(status_active), NULL, NULL);
+        ::draw_text(status_active, target, pos_0, BACKGROUND_BLUE);
     }
     else {
-        SetConsoleTextAttribute(console_handle, BACKGROUND_GREEN | BACKGROUND_RED);
-        SetConsoleCursorPosition(console_handle, { pos.X, (SHORT)(pos.Y + 0) });
-        ::WriteConsoleA(console_handle, status_idle, strlen(status_idle), NULL, NULL); 
+        ::draw_text(
+                status_idle,
+                target,
+                pos_0,
+                BACKGROUND_RED | BACKGROUND_GREEN);
     }
-
-    SetConsoleTextAttribute(console_handle, screen_buffer_info.wAttributes);
 }
 
 void on_exit(HANDLE console_handle, SHORT width, SHORT height) {
@@ -300,8 +340,17 @@ void on_exit(HANDLE console_handle, SHORT width, SHORT height) {
 
 int main(int argc, char *argv[]) {
     char profile[256];
+    char root_path[256];
+
     if (argc >= 2) {
-        ::strcpy(profile, argv[1]);
+        ::strcpy(root_path, argv[1]);
+    }
+    else {
+        ::strcpy(root_path, "");
+    }
+
+    if (argc >= 3) {
+        ::strcpy(profile, argv[2]);
     }
     else {
         ::strcpy(profile, "default");
@@ -321,7 +370,7 @@ int main(int argc, char *argv[]) {
 
     screen_buffer char_0;
 
-    load_characters();
+    load_characters(root_path);
 
     init_buffer(&buffer_0, screen_width, screen_height);
     init_buffer(&buffer_1, screen_width, screen_height);
@@ -342,7 +391,9 @@ int main(int argc, char *argv[]) {
     bool manual_pause = false;
     bool running = true;
     bool paused = false;
+    bool is_inline = false;
     bool prev_paused = false;
+    bool show_seconds = true;
 
     int inactivity_counter = 0;
 
@@ -365,6 +416,12 @@ int main(int argc, char *argv[]) {
             }
             else if (c == 27) {
                 running = false;
+            }
+            else if (c == 's') {
+                show_seconds = !show_seconds;
+            }
+            else if (c == 'i') {
+                is_inline = !is_inline;
             }
         }
 
@@ -415,17 +472,38 @@ int main(int argc, char *argv[]) {
         context.seconds_elapsed = seconds_elapsed_start + elapsed;
 
         char buffer[256];
-        sprintf(
+        if (show_seconds) {
+            sprintf(
                 buffer, 
-                "%02uul:%02uul:%02uul", 
+                "%02u:%02u:%02u", 
                 context.seconds_elapsed / 3600, 
                 (context.seconds_elapsed % 3600) / 60, 
                 context.seconds_elapsed % 60);
+        }
+        else {
+            sprintf(
+                buffer, 
+                "%02u:%02u", 
+                context.seconds_elapsed / 3600, 
+                (context.seconds_elapsed % 3600) / 60);
+        }
         
-        draw_large_text(buffer, front, { 3, 3 });
-        update_screen(console_handle, back, front, force_update);
+        int right_edge;
+        if (!is_inline) {
+            draw_large_text(buffer, front, { 3, 3 }, &right_edge);
+        }
+        else {
+            draw_text(buffer, front, { 1, 0 }, default_color, &right_edge);
+        }
 
-        draw_status_indicators(console_handle, paused, inactivity_counter < 5000, { 75, 3 }); 
+        draw_status_indicators(
+                front,
+                paused,
+                inactivity_counter < 5000,
+                is_inline,
+                { (SHORT)(right_edge + 1), (is_inline) ? (SHORT)0 : (SHORT)3 }); 
+
+        update_screen(console_handle, back, front, force_update);
 
         save_timer_context(&context);
 
